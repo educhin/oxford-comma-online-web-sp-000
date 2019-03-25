fd49e3af689f7b72fab990a230d86b9e549bf7a1
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-25 20:07:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
 def oxford_comma(arr)
 
-  size = arr.size
-  case size
-  when size > 2
+  if arr.size > 2
     arr[-1] = "and #{arr[-1]}"
     arr.join(", ")
-  when size == 2
+  elsif size == 2
     arr[-1] = "and #{arr[-1]}"
     arr.join
   else
